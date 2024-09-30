696b3dc8190aea8e8848dbfeef8bcd7ff25b50ae
Noura <ghamdi7n@gmail.com>
2024-09-30 15:37:01 +0300

    Initial Commit

---
+++ Noura1/Noura1App.swift
@@ -0,0 +1,17 @@
+//
+//  Noura1App.swift
+//  Noura1
+//
+//  Created by Noura  on 27/03/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct Noura1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Noura1Tests/Noura1Tests.swift
@@ -0,0 +1,16 @@
+//
+//  Noura1Tests.swift
+//  Noura1Tests
+//
+//  Created by Noura  on 27/03/1446 AH.
+//
+
+import Testing
+
+struct Noura1Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
